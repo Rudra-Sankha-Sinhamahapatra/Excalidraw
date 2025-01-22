9c39222cf10d593f1004fb35a94d16e0a02f35af
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-22 21:47:55 +0530

    initialized prisma schema and prisma with singleton class

---
+++ packages/db/prisma/migrations/20250122150355_room/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Room" ADD CONSTRAINT "Room_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
