3c2ff8740e702433a1a656c90cf185fef3dec764
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-25 23:41:08 +0530

    room joining using slug and chats getting api by roomid api added

--- apps/http-backend/src/routes/user.ts
+++ apps/http-backend/src/routes/user.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express'
-import { createRoom, signin, signup } from '../controllers/user';
+import { createRoom, getChats, joinRoom, signin, signup } from '../controllers/user';
 import { middleware } from '../middlewares/user';
 
 const router:Router = express.Router();
@@ -7,5 +7,7 @@ const router:Router = express.Router();
 router.post("/signup",signup);
 router.post("/signin",signin);
 router.post("/room",middleware,createRoom);
+router.get("/chats/room/:roomId",middleware,getChats);
+router.post("/room/:slug",middleware,joinRoom);
 
 export default router;
