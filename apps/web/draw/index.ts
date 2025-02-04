b59dbbc9afa7a3056d23b0b97ab17cf9f2e0bf76
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-02-04 23:55:32 +0530

    Multiple objects added on canvas

--- apps/web/draw/index.ts
+++ apps/web/draw/index.ts
@@ -1,5 +1,23 @@
+type Shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      centerX: number;
+      centerY: number;
+      radius: number;
+    };
+
 export function initDraw(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d");
+
+  const existingShapes: Shape[] = [];
+
   if (!ctx) {
     return;
   }
@@ -31,6 +49,16 @@ export function initDraw(canvas: HTMLCanvasElement) {
   canvas.addEventListener("mouseup", (e) => {
     clicked = false;
     const pos = getMousePos(e);
+    const height = pos.y - startY;
+    const width = pos.x - startX;
+
+    existingShapes.push({
+      type: "rect",
+      x: startX,
+      y: startY,
+      height,
+      width,
+    });
     console.log("End Coordinates:", pos.x, pos.y);
   });
 
@@ -39,10 +67,26 @@ export function initDraw(canvas: HTMLCanvasElement) {
       const pos = getMousePos(e);
       const width = pos.x - startX;
       const height = pos.y - startY;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      clearCanvas(existingShapes,canvas,ctx)
+      ctx.strokeStyle = "white"
       ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+}
+
+function clearCanvas(
+  existingShapes: Shape[],
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  existingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeStyle = "white";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
     }
   });
 }
